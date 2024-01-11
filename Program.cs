f844345e67e5dac278788d821304bad43c10f07d
heronet <sirat4757@gmail.com>
2024-01-12 02:49:05 +0600

    test fail 5

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using SustApi.Extensions;
+using SustApi.Utils;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+builder.Services.AddCorsConfiguration();
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,6 +23,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(Constants.CORS_POLICY);
+
 
 app.MapControllers();
 
